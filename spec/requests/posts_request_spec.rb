1a7ced39eba3fd5fef8f8e6d82e63c7d0104f6a7
ichigo-dev <ichigo@web-freework.com>
2021-01-29 17:21:16 +0900

    add rspec posts#index

--- spec/requests/posts_request_spec.rb
+++ spec/requests/posts_request_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
-RSpec.describe "Posts", type: :request do
+RSpec.describe 'Posts', type: :request do
+  context 'GET: /posts' do
+    before do
+      FactoryBot.create_list(:post, 10)
+      get '/api/v1/posts'
+      @json = JSON.parse(response.body)
+    end
 
+    it 'status == 200' do
+      expect(response.status).to eq(200)
+    end
+
+    it 'all post' do
+      expect(@json['data'].length).to eq(10)
+    end
+  end
 end
