ea681ae84880a58caceb83a5cd74176e7b2b232d
ichigo-dev <ichigo@web-freework.com>
2021-01-29 15:04:34 +0900

    add post test

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'is valid with title' do
+    post = Post.new(title: 'hoge')
+    expect(post).to be_valid
+  end
+  it 'is invalid with no title' do
+    post = Post.new()
+    post.valid?
+    expect(post.errors[:title]).to include("can't be blank")
+  end
 end
